f1f177401ebbe17c06e3d1d21b1ed0d9e3ec7046
Matthew Jackoski <mjackoski@Matthews-MacBook-Pro.local>
2020-04-08 22:55:17 -0700

    seperated out actions class

---
+++ Actions.py
@@ -0,0 +1,38 @@
+import requests as req
+class Actions:
+
+    def __init__(self,message_txt):
+        self.text=message_txt.get('text')
+        c_pos=self.text.find(' ')
+        c_pos2=self.text.find(' ',c_pos+1)
+        if c_pos2==(-1):
+            c_pos2=len(self.text)
+        self.command = self.text[c_pos+1:c_pos2]
+        if "files" in message_txt:
+            self.files=message_txt['files']
+    
+    def addioc(tc_url):
+        pos1=self.text.find('`')
+        pos2=self.text.find('`',pos1+1)
+        ioc=self.text[pos1+1:pos2]
+        res=req.post(tc_url,data=ioc)
+        if res.status_code==200:
+            return True
+        else:
+            return False
+
+    def bulkadd(tc_url,token):
+        f=self.files
+        iocs=""
+        file_down_fail=False
+        for i in f:
+            res=req.get(i['url_private_download'], headers={"Authorization": "Bearer "+token})
+            iocs+=res.txt
+            if res.status_code!=200:
+                return False
+        if file_down_fail==False:
+            res=req.post(self.tc_url,data=iocs)
+            if res.status_code==200:
+                return True
+            else:
+                return False
--- __init__.py
+++ __init__.py
@@ -4,9 +4,9 @@ from flask import Flask
 import certifi
 import requests as req
 import ssl as ssl_lib
-from Responses import Responses
 import logging
 import enviroment
+from Actions import Actions
 
 app = Flask(__name__)
 
@@ -21,44 +21,22 @@ domain_tc_endpoint=enviroment.DOMAIN_TC
 @slack_events_adapter.on("app_mention")
 def message(event_data):
     message_txt=event_data["event"]
-    logger.debug(event_data["event"])
-    text = message_txt.get('text')
-    c_pos=text.find(' ')
-    c_pos2=text.find(' ',c_pos+1)
-    if c_pos2==(-1):
-        c_pos2=len(text)
-    logger.debug(c_pos)
-    logger.debug(c_pos2)
-    command = text[c_pos+1:c_pos2]
-    logger.debug(command)
-    if "addioc" ==command or "addio"==command:
-        ioc=text[c_pos2+1:]
-        res=req.post(domain_tc_endpoint,data=ioc)
-        if res.status_code==200:
+    act=Actions(message_txt)
+    # resp=response(message_txt['channel'],message_txt['user'])
+    if "addioc" ==act.command or "addio"==act.command:
+        succ=act.addioc(enviroment.DOMAIN_TC)
+        if succ:
             slack_client.chat_postMessage(channel=message_txt['channel'],text='IOC was submitted!')
         else:
             slack_client.chat_postMessage(channel=message_txt['channel'],text='Error submitting IOC')
         #send request
-    elif "bulkad" == command or "bulkadd"==command:
-        f=message_txt['files']
-        iocs=""
-        file_down_fail=False
-        for i in f:
-            logger.debug(i['url_private_download'])
-            res=req.get(i['url_private_download'], headers={"Authorization": "Bearer "+token})
-            logger.debug(res.text)
-            iocs+=res.txt
-            if res.status_code!=200:
-                slack_client.chat_postMessage(channel=message_txt['channel'],text='Error reading file.')
-                file_down_fail=True
-                break
-        if file_down_fail==False:
-            res=req.post(domain_tc_endpoint,data=iocs)
-            if res.status_code==200:
-                slack_client.chat_postMessage(channel=message_txt['channel'],text='IOC was submitted!')
-            else:
-                slack_client.chat_postMessage(channel=message_txt['channel'],text='Error submitting IOC')
-    elif "hello" == command or "hi" == command or "help" == command:
+    elif "bulkad" == act.command or "bulkadd"==act.command:
+        succ=act.men.bulkadd(enviroment.DOMAIN_TC,token)
+        if succ:
+            slack_client.chat_postMessage(channel=message_txt['channel'],text='IOC was submitted!')
+        else:
+            slack_client.chat_postMessage(channel=message_txt['channel'],text='Error submitting IOC')
+    elif "hello" == act.command or "hi" == act.command or "help" == act.command:
         slack_client.chat_postMessage(channel=message_txt['channel'],blocks=[
             {
                 "type":"section",
