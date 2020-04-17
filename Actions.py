b6769ef01b38852818382238b37d314db1aa2f62
Paul Salessi <paulsalessi@gmail.com>
2020-04-16 23:00:34 -0700

    Add falsepositives command. Environment was spelled wrong so also fix that. Fix some formatting

--- Actions.py
+++ Actions.py
@@ -3,75 +3,98 @@ import logging
 import string
 import re
 import responses
-import enviroment
-
+import environment
 
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 
-
-def format_domains(ioc):
-        return '{"url":"'+ioc+'"}'
+# def format_domains(ioc):
+#         return f'{'url':''+ioc+''}'
 
 class Actions:
-
-    def __init__(self,message_txt,text):
-        self.message=message_txt
-        self.channel=message_txt['channel']
-        self.user=message_txt['user']
-        self.text=text
-        # self.text=message_txt.get('blocks')[0].get('elements')[0].get('elements')[1].get('text')
+    def __init__(self, message_txt, text):
+        self.message = message_txt
+        self.channel = message_txt['channel']
+        self.user = message_txt['user']
+        self.text = text
+        # self.text = message_txt.get('blocks')[0].get('elements')[0].get('elements')[1].get('text')
         # logger.debug(message_txt.get('elements'))
-        if "addioc" in self.text:
-            self.command= "addioc"
-        elif "bulkadd" in self.text:
-            self.command="bulkadd"
-        elif "hi" in self.text or "help" in self.text or "hello" in self.text:
-            self.command="help"
-            responses.send_help(self.channel,self.user)
+
+        if 'addioc' in self.text:
+            self.command = 'addioc'
+        elif 'bulkadd' in self.text:
+            self.command = 'bulkadd'
+        elif 'hi' in self.text or 'help' in self.text or 'hello' in self.text:
+            self.command = 'help'
+            responses.send_help(self.channel, self.user)
+        elif 'falsepositive' in self.text:
+            self.command = 'falsepositive'
         else:
-            self.command="unknown"
-            responses.send_unknown(channel)
-        logger.debug('COMMAND: '+self.command)
-        if "files" in message_txt:
-            self.files=message_txt['files']
-    
-    def addioc(self,tc_url,ioc):
-        if "[.]" in ioc:
-            ioc=ioc.replace("[.]",".")
+            self.command = 'unknown'
+            responses.send_unknown(self.channel)
+        logger.debug(f'COMMAND: {self.command}')
+
+        if 'files' in message_txt:
+            self.files = message_txt['files']
+
+
+    def addioc(self, tc_url, ioc):
+        if '[.]' in ioc:
+            ioc=ioc.replace('[.]','.')
+
         ioc=ioc.strip('<')
         ioc=ioc.strip('>')
-        logger.debug("IOC: "+ioc)
+        logger.debug('IOC: '+ioc)
+
         res=req.post(tc_url,data=ioc)
         if res.status_code==200:
             responses.send_success_message(self.channel)
         else:
             responses.send_failure(self.channel)
-    
-    def bulkadd(self,tc_url):
+
+
+    def bulkadd(self, tc_url):
         f=self.files
-        iocs=""
-        file_down_fail=False
+        iocs=''
+        # file_down_fail=False
+
         for i in f:
-            logger.debug('URL DOWNLOAD: '+i['url_private_download'])
-            res=req.get(i['url_private_download'], headers={"Authorization": "Bearer "+enviroment.TOKEN})
+            logger.debug(f"URL DOWNLOAD: {i['url_private_download']}")
+            res=req.get(i['url_private_download'], headers={'Authorization': f'Bearer {environment.TOKEN}'})
             if res.text not in iocs:
                 iocs+=res.text
+
             if res.status_code!=200:
                 responses.send_failure(self.channel)
                 return
+
         ioc_array=iocs.split('\n')
-        logger.debug('Attempting to submit '+str(len(ioc_array))+' IOCS...')
+        logger.debug(f'Attempting to submit {str(len(ioc_array))} IOCS...')
         responses.send_ioc_count(self.channel,len(ioc_array))
         count=0
         for i in ioc_array:
             res=req.post(tc_url,data=i)
             if res.status_code!=200:
-                logger.debug("Upload Failure:\n"+res.text)
+                logger.debug(f'Upload Failure:\n {res.text}')
                 responses.send_failure(self.channel)
                 return
             count+=1
-        logger.debug('Number of IOCs submitted: '+str(count))
+
+        logger.debug(f'Number of IOCs submitted: {str(count)}')
         responses.send_success_message(self.channel)
 
+
+    def falsepositive(self, fp_ioc):
+        if '[.]' in fp_ioc:
+            fp_ioc = fp_ioc.replace('[.]','.')
+
+        fp_ioc = fp_ioc.strip('<')
+        fp_ioc = fp_ioc.strip('>')
+        logger.debug(f'False PositiveIOC: {fp_ioc}')
+        responses.send_message_to_slack(self.channel, 'False Positive was submitted!')
+        # res=req.post(data=ioc)
+        # if res.status_code==200:
+        #     responses.send_success_message(self.channel)
+        # else:
+        #     responses.send_failure(self.channel)
--- slack_tc.py
+++ slack_tc.py
@@ -4,43 +4,55 @@ import certifi
 import requests as req
 import ssl as ssl_lib
 import logging
-import enviroment
+import environment
 from Actions import Actions
 import responses
 
 application = Flask(__name__)
+slack_events_adapter=SlackEventAdapter(environment.SECRET,'/slack/events',application)
+domain_tc_endpoint=environment.DOMAIN_TC
 
-slack_events_adapter=SlackEventAdapter(enviroment.SECRET,"/slack/events",application)
 
-domain_tc_endpoint=enviroment.DOMAIN_TC
+@slack_events_adapter.on('app_mention')
+def app_mention(event_data):
+    message_txt = event_data['event']
+    logger.debug(message_txt)
+    text = message_txt.get('blocks')[0].get('elements')[0].get('elements')[1].get('text')
+    act = Actions(message_txt,text)
+    if 'addioc' == act.command or 'addio' == act.command:
+        ioc = message_txt.get('blocks')[0].get('elements')[0].get('elements')[2].get('url')
+        act.addioc(environment.DOMAIN_TC, ioc)
+    elif 'bulkad' == act.command or 'bulkadd' == act.command:
+        act.bulkadd(environment.DOMAIN_TC)
+    elif 'falsepositive' == act.command:
+        # TODO pass Threat connect endpoint
+        fp_ioc = message_txt.get('blocks')[0].get('elements')[0].get('elements')[2].get('url')
+        act.falsepositive(fp_ioc)
 
-@slack_events_adapter.on("app_mention")
+
+
+@slack_events_adapter.on('message')
 def message(event_data):
-    message_txt=event_data["event"]
-    text=message_txt.get('blocks')[0].get('elements')[0].get('elements')[1].get('text')
-    act=Actions(message_txt,text)
-    if "addioc" ==act.command or "addio"==act.command:
-        ioc=message_txt.get('blocks')[0].get('elements')[0].get('elements')[2].get('url')
-        succ=act.addioc(enviroment.DOMAIN_TC)
-    elif "bulkad" == act.command or "bulkadd"==act.command:
-        succ=act.bulkadd(enviroment.DOMAIN_TC)
+    message_txt = event_data['event']
+    logger.debug(message_txt)
+    if 'bot_id' not in message_txt:
+        text = message_txt.get('blocks')[0].get('elements')[0].get('elements')[0].get('text')
+        act = Actions(message_txt,text)
+        # resp = response(message_txt['channel'],message_txt['user'])
+        if 'addioc' == act.command or 'addio' == act.command:
+            ioc = message_txt.get('blocks')[0].get('elements')[0].get('elements')[1].get('url')
+            act.addioc(environment.DOMAIN_TC, ioc)
+        elif 'bulkad' == act.command or 'bulkadd' == act.command:
+            act.bulkadd(environment.DOMAIN_TC)
+        elif 'falsepositive' == act.command:
+            # TODO pass Threat connect endpoint
+            fp_ioc = message_txt.get('blocks')[0].get('elements')[0].get('elements')[1].get('url')
+            act.falsepositive(fp_ioc)
 
-@slack_events_adapter.on("message")
-def message(event_data):
-    message_txt=event_data["event"]
-    if "bot_id" not in message_txt:
-        text=message_txt.get('blocks')[0].get('elements')[0].get('elements')[0].get('text')
-        act=Actions(message_txt,text)
-        # resp=response(message_txt['channel'],message_txt['user'])
-        if "addioc" ==act.command or "addio"==act.command:
-            ioc=message_txt.get('blocks')[0].get('elements')[0].get('elements')[1].get('url')
-            succ=act.addioc(enviroment.DOMAIN_TC,ioc)
-        elif "bulkad" == act.command or "bulkadd"==act.command:
-            succ=act.bulkadd(enviroment.DOMAIN_TC)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
+
     ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
-    application.run(port=5000,host='0.0.0.0')
+    application.run(port=5000, host='0.0.0.0')
