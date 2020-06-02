cf58023cf0f5c50d9ed502927ed8e87f645e566a
Matthew Jackoski <mjackoski@Matthews-MacBook-Pro.local>
2020-06-01 18:50:13 -0700

    one more adding ioc error

--- Actions.py
+++ Actions.py
@@ -188,7 +188,7 @@ class Action:
 
         for i in ioc_array:
             res = req.post(tc_url, data=i)
-            if res.ok:
+            if not res.ok:
                 logger.debug(f'Upload Failure:\n {res.text}')
                 responses.send_failure(self._channel)
                 return
