00586e9453fc7eb023fc55f49226ea2dc47845af
Matthew Jackoski <mjackoski@Matthews-MacBook-Pro.local>
2020-06-09 12:42:41 -0700

    adding schedule to bulkadd helper

--- bulk_add_helper.py
+++ bulk_add_helper.py
@@ -2,6 +2,9 @@ import requests as req
 import logging
 import environment
 import time
+import schedule
+import time
+
 
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -10,7 +13,7 @@ logger.addHandler(logging.StreamHandler())
 
 tc_url = environment.DOMAIN_TC
 
-def submit():
+def job():
     with open('to_submit.txt','r') as f:
         for i in f:
             count=0
@@ -23,3 +26,10 @@ def submit():
             logger.debug(f'Uploaded: {i}')
             time.sleep(1)
     open('to_submit.txt','w').close()
+
+schedule.every(10).minutes.do(job)
+
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
