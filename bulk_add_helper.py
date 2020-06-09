3bf1078b1ed7112fd257508b938f342762c6c031
Matthew Jackoski <mjackoski@Matthews-MacBook-Pro.local>
2020-06-09 12:38:37 -0700

    adding bulk add helper

---
+++ bulk_add_helper.py
@@ -0,0 +1,25 @@
+import requests as req
+import logging
+import environment
+import time
+
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+
+tc_url = environment.DOMAIN_TC
+
+def submit():
+    with open('to_submit.txt','r') as f:
+        for i in f:
+            count=0
+            res = req.post(tc_url, data=i)
+            logger.debug(f"Result status code: {res.status_code}")
+            if not res.ok:
+                logger.debug(f'Upload Failure:\n {res.text}')
+                return
+            count += 1
+            logger.debug(f'Uploaded: {i}')
+            time.sleep(1)
+    open('to_submit.txt','w').close()
